799b7165c520b9f315f55e4ad95b6df880f09942
JacksonZheng07 <Jacksonzheng425@gmail.com>
2026-03-23 14:34:58 -0400

    added all components of Next.js and fixed some linting error and now testing the github action workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    // Next typegen artifacts can contain framework-owned fallbacks like `any`.
+    "types/**",
+  ]),
+]);
+
+export default eslintConfig;
