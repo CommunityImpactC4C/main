2a1c5e5eb2bb2b4f71898f6588e3a39805fa3e3b
Yash Maheshwari <ymaheshwari2007@gmail.com>
2026-03-29 13:27:22 -0400

    finished git library with error handling

--- src/lib/gemeni.ts
+++ src/lib/gemeni.ts
@@ -1,25 +1,26 @@
 import { GoogleGenAI } from "@google/genai";
 
-if (!process.env.GEMINI_API_KEY) {
-  throw new Error("Missing GEMINI_API_KEY in .env file");
-}
+
+
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const apiKey = process.env.GEMINI_API_KEY
 
-const ai = new GoogleGenAI({apiKey});
+const ai = process.env.GEMINI_API_KEY ? 
+  new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY }): null;
 
 
-
-async function Gemeni(prompt:string) {
-  const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
+async function Gemeni(prompt:string, model:string = "gemini-3-flash-preview", config:object = {
+    temperature: 0.2,
+    responseMimeType: "application/json",
+    },) 
+    {
+    if (!ai) {
+      throw new Error("Missing GEMINI_API_KEY in .env file");
+    }
+    const response = await ai.models.generateContent({
+    model: model,
     contents: prompt,
-    config: {
-      temperature: 0.1,
-      responseMimeType: "application/json",
-      // responseJsonSchema: zodToJsonSchema(recipeSchema),
-    },
+    config: config,
   });
   return response;
 }
