b4668e4e49d5e80fc7727a50035fd36225d0502e
Yash Maheshwari <ymaheshwari2007@gmail.com>
2026-03-30 21:33:02 -0400

    fixed the seed files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,12 @@
 import "dotenv/config";
-import prisma from "../src/lib/prisma";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../src/generated/prisma/client";
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 
 async function main() {
